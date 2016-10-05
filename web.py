4c8be2616c12e6e5868c31e2b7ccedf3ee6a928a
Rich Larin <RLarin17@gsb.columbia.edu>
2016-10-05 16:46:08 -0400

    Created an about page

--- web.py
+++ web.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 def index(): #this needs to be right under the @app line
 	return render_template('index.html')
 
+@app.route('/about')
+def about(): #this needs to be right under the @app line
+	return render_template('about.html')
+
 app.run(debug=True)
